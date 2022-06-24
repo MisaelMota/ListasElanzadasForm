0ef7d08fab30d714f111a9020be4752a34f44028
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2022-06-23 23:08:32 -0400

    Add project files.

---
+++ ListasElanzadasForm/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace ListasElanzadasForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnAddFirst = new System.Windows.Forms.Button();
+            this.btnAddLast = new System.Windows.Forms.Button();
+            this.btnMoverFinal = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this._Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this._Name});
+            this.dataGridView1.Location = new System.Drawing.Point(292, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 391);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 50);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(124, 37);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(12, 142);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(98, 42);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar Alumnos";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFirst
+            // 
+            this.btnAddFirst.Location = new System.Drawing.Point(12, 190);
+            this.btnAddFirst.Name = "btnAddFirst";
+            this.btnAddFirst.Size = new System.Drawing.Size(98, 42);
+            this.btnAddFirst.TabIndex = 5;
+            this.btnAddFirst.Text = "Add First";
+            this.btnAddFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnAddLast
+            // 
+            this.btnAddLast.Location = new System.Drawing.Point(12, 238);
+            this.btnAddLast.Name = "btnAddLast";
+            this.btnAddLast.Size = new System.Drawing.Size(98, 42);
+            this.btnAddLast.TabIndex = 6;
+            this.btnAddLast.Text = "Add Last";
+            this.btnAddLast.UseVisualStyleBackColor = true;
+            // 
+            // btnMoverFinal
+            // 
+            this.btnMoverFinal.Location = new System.Drawing.Point(12, 286);
+            this.btnMoverFinal.Name = "btnMoverFinal";
+            this.btnMoverFinal.Size = new System.Drawing.Size(98, 42);
+            this.btnMoverFinal.TabIndex = 7;
+            this.btnMoverFinal.Text = "Mover Al final";
+            this.btnMoverFinal.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(12, 334);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 42);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(153, 50);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(62, 37);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // _Name
+            // 
+            this._Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this._Name.DataPropertyName = "Nombre";
+            this._Name.HeaderText = "Name";
+            this._Name.Name = "_Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnMoverFinal);
+            this.Controls.Add(this.btnAddLast);
+            this.Controls.Add(this.btnAddFirst);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnAddFirst;
+        private System.Windows.Forms.Button btnAddLast;
+        private System.Windows.Forms.Button btnMoverFinal;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Name;
+    }
+}
+
---
+++ ListasElanzadasForm/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasElanzadasForm
+{
+    public partial class Form1 : Form
+    {
+
+        public LinkedList<Form1> Lalumnos = new LinkedList<Form1>();
+        public string Nombre { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnAddFirst.Click += BtnAddFirst_Click;
+            btnAddLast.Click += BtnAddLast_Click;
+            btnMostrar.Click += BtnMostrar_Click;
+            btnMoverFinal.Click += BtnMoverFinal_Click;
+            btnEliminar.Click += BtnEliminar_Click;
+            btnBuscar.Click += BtnBuscar_Click;
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            SiExisteAlumno();
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            EliminarAlumno();
+        }
+
+        private void BtnMoverFinal_Click(object sender, EventArgs e)
+        {
+            MoverAlumnosFinal();
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            MostrarAlumnos();
+        }
+
+        private void BtnAddLast_Click(object sender, EventArgs e)
+        {
+            AgregarAlumnosLast();
+        }
+
+        private void BtnAddFirst_Click(object sender, EventArgs e)
+        {
+            AgregarAlumnosFirst();
+        }
+
+        public void AgregarAlumnosFirst()
+        {
+           
+               Form1 alumno = new Form1();               
+                           
+               alumno.Nombre = txtName.Text;
+               Lalumnos.AddFirst(alumno);
+            dataGridView1.Rows.Clear();
+            foreach (var item in Lalumnos)
+            {
+                dataGridView1.Rows.Add(
+                  item.Nombre
+                  );
+
+            }
+
+
+        }
+
+        public void AgregarAlumnosLast()
+        {
+            Form1 alumno = new Form1();
+            alumno.Nombre = txtName.Text;
+            Lalumnos.AddLast(alumno);
+            dataGridView1.Rows.Clear();
+            foreach (var item in Lalumnos)
+            {
+                dataGridView1.Rows.Add(
+                  item.Nombre
+                  );
+
+            }
+        }
+
+
+        public void MostrarAlumnos()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var item in Lalumnos)
+            {
+                dataGridView1.Rows.Add(
+                  item.Nombre
+                  );
+            }
+
+
+
+        }
+
+        public void MoverAlumnosFinal()
+        {
+            Form1 alumno = new Form1();
+            alumno.Nombre = txtName.Text;
+
+            var cambiarAlumno = Lalumnos.FirstOrDefault(a => a.Nombre.Equals(alumno.Nombre));
+            Lalumnos.Remove(cambiarAlumno);
+            Lalumnos.AddLast(alumno);
+
+            MostrarAlumnos();
+
+
+        }
+
+        public void EliminarAlumno()
+        {
+            Form1 alumno = new Form1();
+            alumno.Nombre = txtName.Text;
+
+            var cambiarAlumno = Lalumnos.FirstOrDefault(a => a.Nombre.Equals(alumno.Nombre));
+            Lalumnos.Remove(cambiarAlumno);
+            MostrarAlumnos();
+        }
+
+
+        public void SiExisteAlumno()
+        {
+            Form1 alumno = new Form1();
+            string Nombre =txtName.Text;
+
+         
+            foreach (var item in Lalumnos)
+            {
+                if (Nombre==item.Nombre)
+                {
+                    dataGridView1.Rows.Clear();
+                 dataGridView1.Rows.Add(
+                 item.Nombre
+                 );
+                    break;
+
+                }
+                
+            }
+
+        }
+
+
+
+
+
+    }
+}
